0faf776ab663708d01e958311e25f593bf72fb4f
ukasha <ukasha909@outlook.com>
2019-12-04 21:29:11 +0500

    v0.1 added buttons and 1 question

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,37 @@ void main() => runApp(MyApp());
 
 // main class
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      "What's your favorite Color?",
+      "What's your favorite Animal? ",
+    ];
 
-    return MaterialApp(home: Text("Hello!!!"),);
-  
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Quiz App"),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text("The Qustion"),
+            RaisedButton(
+              child: Text("Answer 1"),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text("Answer 2"),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text("Answer 3"),
+              onPressed: null,
+            ),
+          ],
+        ), //if we want more than one widget
+        //then we use Column  /// list of widgets
+      ),
+    ); //scaffold gives theme
   }
 }
