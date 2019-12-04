ea3c0ecb0503404521603af37db46e2bb568cddf
ukasha <ukasha909@outlook.com>
2019-12-04 21:05:50 +0500

    v0.0

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+// main function
+void main() => runApp(MyApp());
+
+// main class
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(home: Text("Hello!!!"),);
+  
+  }
+}
